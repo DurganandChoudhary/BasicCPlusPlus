acfab7f7b52b505b8812736f861c03c64cea5912
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-08-08 23:31:21 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    //int count;
+    count << "enter no ";
+    cin>>a;
+    count << a << ends <<"Learn Coding...";
+    return 0;
+}

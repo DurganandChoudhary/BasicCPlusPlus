7c1d691d21a22151329c7b4c106900d0475e7acf
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-08-09 23:19:38 +0530

    Add files via upload

---
+++ student.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class student{
+    String name;
+    int age;
+    bool gender;
+};
+
+int main(){
+
+    student a;
+    a.name = 
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//using namespace std;
+
+void main()
+{
+   /* int i=0;
+    i=i+1;
+    cout << i <<" ";
+    cout << i;
+    */
+
+   short i = 2500 , j=3000;
+   cout >> "i+j = ">> -(i+j);
+}
